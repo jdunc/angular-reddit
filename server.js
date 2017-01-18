5c6f27b4595da549af0cd166b59526124420c4d5
Jordan Duncan <j.duncan@rochester.edu>
2017-01-18 11:35:21 -0600

    did form validation warmup using custom directives

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 const port = process.env.PORT || 8000;
 // Define the port to run on
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'warmup')));
 
 // Listen for requests
 app.listen(port, () => {
